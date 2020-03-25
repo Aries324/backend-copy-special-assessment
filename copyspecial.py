b6a6e825e1ab490af1a3289490399ef461ccb592
Tiffany McLean <tamclean324@gmail.com>
2020-03-25 01:12:01 -0400

    correects assessment

--- copyspecial.py
+++ copyspecial.py
@@ -8,69 +8,80 @@
 
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
-import sys
 import re
 import os
 import shutil
 import subprocess
 import argparse
-from argparse import RawTextHelpFormatter
+
 
 """Copy Special exercise
 """
 
 # +++your code here+++
 # Write functions and modify main() to call them
-__author__ = "Sarah Gale"
+__author__ = "Tiffany McLean with the help of demo"
+
+"""
+create a helper function
+"""
 
 
-def is_special_path(a_path):
-    """
-    a_path may be a file or a directory
-    if path is not special, return False
-    if path is special, return True
-    """
-    match = re.search(r'__\w+__', a_path)
-    if match is None:
-        is_special = False
+def get_special_paths(dir):
+    """Manipulate file path"""
+    special_paths = []
+    files = os.listdir(dir)
+    for file in files:
+        if re.search(r'__\w+__', file):
+            special_paths.append(file)
+    return special_paths
+
+
+def copy_to(path, files):
+    """This will copy special files to a directory"""
+    if not os.path.exists(path):
+        os.makedirs(path)
     else:
-        is_special = True
-    return is_special
+        print('Path exists')
+
+    for file in files:
+        shutil.copy(file, path)
 
 
-def get_special_paths(a_dir):
-    """
-    return a list of the absolute paths of the special files in the given directory
-    """
-    filenames = os.listdir(a_dir)
-    file_list = []
-    for filename in filenames:
-        if ((not os.path.isdir(filename)) and is_special_path(filename)):
-            a_path = os.path.join(a_dir, filename)
-            absolute_path = os.path.abspath(a_path)
-            file_list.append(absolute_path)
-    return file_list
+def zip_to(paths, zippath):
+    """ Creates zipfiles from specialfiles"""
+    paths = list(paths)
+    command = 'zip -j {} {}'.format(zippath, ' '.join(paths))
+    print(command)
+    # runs the command in the terminal as if you just typed it into the terminal
+    os.system(command)
 
 
-def get_special_paths_in_dirs(a_dirs):
-    """
-    return a list of the absolute paths of the special files in a list of directories
-    """
-    file_list = []
-    for dirname in a_dirs:
-        files_in_dirname = get_special_paths(dirname)
-        # extend, not append
-        file_list.extend(files_in_dirname)
-    return file_list
+def zip_to_two(paths, zippath):
+    command = ['zip', '-j', zippath]
+    command.extend(paths)
+    print('Command I am going to do: {}'.format(' '.join(command)))
+    subprocess.check_output(command)
 
 
-def copy_to(paths, a_dir):
-    """
-    given a list of paths, copies those files into the given directory
-    specification says use shutil
-    """
-    for path in paths:
-        # copytree will copy a directory, not a file
-        #shutil.copytree(path, a_dir)
-        # copy2 will copy a file, not a directory
-        shutil.copy2(path, a_dir)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--todir', help='dest dir for special files')
+    parser.add_argument('--tozip', help='dest zipfile for special files')
+    parser.add_argument('fromdir', help='src dir to look for local files')
+    args = parser.parse_args()
+
+    all_paths = get_special_paths(args.fromdir)
+
+    if args.todir:
+        copy_to(args.todir, all_paths)
+
+    if args.tozip:
+        zip_to_two(all_paths, args.tozip)
+
+    if not args.todir and not args.tozip:
+        print('\n'.join(all_paths))
+
+
+if __name__ == "__main__":
+    main()
